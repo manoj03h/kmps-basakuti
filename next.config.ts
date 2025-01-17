8c460946693ecad26d7f7235174eee66d96cbab9
Manoj Hembram <manoj03h@gmail.com>
2025-01-17 09:09:55 +0530

    updated website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  exlint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
